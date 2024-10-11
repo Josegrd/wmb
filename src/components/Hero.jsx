85ea910cc968b82789fa2084d2b7b0a824de4ed6
Jose Garda Seta <jose8gard@gmail.com>
2024-10-11 16:01:31 +0700

    [FIX] clear comments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,4 @@
 import ImageHeroes from "../assets/hero-image.jpg";
-// import AstraImage from "../assets/logo-astrapay.png";
-// import ShopeeImage from "../assets/shopee-pay-logo.png";
-// import OvoImage from "../assets/logo-ovo.png";
-// import LinkajaImage from "../assets/logo-linkaja.png";
-// import JeniusImage from "../assets/ewallet_jenius-pay.png";
-import ReactTypingEffect from "react-typing-effect";
-// import Swiper from "swiper";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Autoplay, Pagination } from "swiper/modules";
-
-// import "swiper/css";
-// import "swiper/swiper-bundle.css";
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
 
 export default function Hero() {
   return (
@@ -38,7 +24,6 @@ export default function Hero() {
                   Welcome to{" "}
                 </span>{" "}
                 <span className="text-[#FD8B51] text-5xl lg:text-6xl">
-                  {/* <ReactTypingEffect text={["WMB", "Warung Makan Bahari!"]} /> */}
                   <h1>Warung Makan Bahari!</h1>
                 </span>
               </h1>
@@ -54,92 +39,9 @@ export default function Hero() {
                 >
                   <a href="/login">Login To Reserve</a>
                 </button>
-                {/* <button
-                  type="button"
-                  className="lg:w-52 w-24 lg:text-2xl text-[17px] lg:py-2.5 outline outline-[#84CECF] rounded-full text-center mb-2 hover:bg-[#84CECF] hover:text-[#150E5E] transition-colors duration-300"
-                >
-                  <a href="/register">Register</a>
-                </button> */}
               </div>
             </div>
           </div>
-
-          {/* <div className="w-full lg:-mt-16 pt-16 ">
-            <h1 className="font-sans text-[20px] font-semibold pb-3 opacity-50 ms-3">
-              CONNECT ON
-            </h1>
-            <div className="relative w-full lg:h-32 rounded-2xl">
-              <div className="absolute inset-0 lg:bg-[#150E5E] lg:opacity-20 rounded-2xl"></div>
-              <div className="relative flex lg:flex-row lg:justify-evenly lg:items-center h-full w-full text-center gap-5">
-                <Swiper
-                  spaceBetween={50}
-                  loop={true}
-                  autoplay={{
-                    delay: 1500,
-                    disableOnInteraction: false,
-                  }}
-                  breakpoints={{
-                    640: {
-                      slidesPerView: 1,
-                    },
-                    768: {
-                      slidesPerView: 3,
-                    },
-                    1024: {
-                      slidesPerView: 4,
-                    },
-                  }}
-                  modules={[Autoplay, Pagination]}
-                >
-                  <SwiperSlide>
-                    <div className="lg:w-48 w-full">
-                      <img
-                        src={AstraImage}
-                        alt="Astra"
-                        className="object-cover mx-auto"
-                      />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="lg:w-48 w-full">
-                      <img
-                        src={ShopeeImage}
-                        alt="Shopee"
-                        className="mx-auto w-56"
-                      />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="lg:w-48 w-full">
-                      <img
-                        src={OvoImage}
-                        alt="OVO"
-                        className="object-cover mx-auto"
-                      />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="lg:w-48 w-full">
-                      <img
-                        src={LinkajaImage}
-                        alt="LinkAja"
-                        className="object-cover mx-auto"
-                      />
-                    </div>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <div className="lg:w-48 w-full">
-                      <img
-                        src={JeniusImage}
-                        alt="Jenius"
-                        className="object-cover mx-auto"
-                      />
-                    </div>
-                  </SwiperSlide>
-                </Swiper>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </>
